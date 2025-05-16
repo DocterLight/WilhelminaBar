ce51d2cfc485d8672ca04c0c456ca68fd9e0167c
DocterLight <48608216+DocterLight@users.noreply.github.com>
2025-05-16 18:13:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,8 @@ const FILES_TO_CACHE = [
   './favicon.png',
   './manifest.json',
   'https://wilhelminaleens.nl/wp-content/gallery/jubileumconcert-2017-85-jaar/Wilhelmina-Jubileumconcert-2017-152.jpg',
-  'https://i0.wp.com/wilhelminaleens.nl/wp-content/uploads/2023/12/logo-Wilhelmina.png?fit=1920%2C976&ssl=1'
+  'https://i0.wp.com/wilhelminaleens.nl/wp-content/uploads/2023/12/logo-Wilhelmina.png?fit=1920%2C976&ssl=1',
+  './offline.html'
 ];
 
 // Installeer en cache bestanden
@@ -49,12 +50,13 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// Vang fetch requests op
+// Fetch event om offline functionaliteit te ondersteunen
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request).catch(() => {
-        console.warn('[ServiceWorker] Fetch failed:', event.request.url);
+        // Als netwerkfout optreedt, geef de offlinepagina terug
+        return caches.match('./offline.html');
       });
     })
   );
